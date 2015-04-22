fdd62c093960e603db1cc38c4b5c7e43ea55fb89
Yifan Lu <me@yifanlu.com>
2015-04-21 21:09:06 -0700

    Added console swapping to export/import seed

--- 3DSExportImportSeed/README.md
+++ 3DSExportImportSeed/README.md
@@ -1,4 +1,4 @@
-# Export/Import Movable SEED
+# Move Console or Export/Import Movable SEED
 
 Uses [service-patch](https://github.com/archshift/service-patch) and [libkhax](https://github.com/Myriachan/libkhax) to do manual system transfer.
 
@@ -9,5 +9,7 @@ On your new system, place "export_seed.bin" on your SD card and run this tool.
 You may want to format your system first. Press select to do so. There is no confirmation prompt! Pressing select WILL format your system to factory settings.
 
 Press Y to import the seed.
+
+Press A to import SecureInfo (extracted from another console's NAND dump)
 
 Turn the system off.
--- 3DSExportImportSeed/source/main.cpp
+++ 3DSExportImportSeed/source/main.cpp
@@ -12,6 +12,92 @@
 #define log(...) fprintf(stderr, __VA_ARGS__)
 
 extern "C" Handle fsuHandle;
+static Handle cfgHandle;
+
+static Result CFG_SetGetLocalFriendCodeSeedData(u8 *data)
+{
+  Result ret = 0;
+  u32 *cmdbuf = getThreadCommandBuffer();
+
+  cmdbuf[0] = 0x080B0082;
+  cmdbuf[1] = 0x10;
+  cmdbuf[2] = 0;
+  cmdbuf[3] = (0x10 << 4) | 12;
+  cmdbuf[4] = (u32)data;
+
+  if((ret = svcSendSyncRequest(cfgHandle))!=0) return ret;
+  
+  return (Result)cmdbuf[1];
+}
+
+static Result CFG_SetLocalFriendCodeSeedSignature(const u8 *data, u32 size)
+{
+  Result ret = 0;
+  u32 *cmdbuf = getThreadCommandBuffer();
+
+  cmdbuf[0] = 0x080C0042;
+  cmdbuf[1] = size;
+  cmdbuf[2] = (size << 4) | 10;
+  cmdbuf[3] = (u32)data;
+
+  if((ret = svcSendSyncRequest(cfgHandle))!=0) return ret;
+  
+  return (Result)cmdbuf[1];
+}
+
+static Result CFG_VerifySigLocalFriendCodeSeed(void)
+{
+  Result ret = 0;
+  u32 *cmdbuf = getThreadCommandBuffer();
+
+  cmdbuf[0] = 0x080E0000;
+
+  if((ret = svcSendSyncRequest(cfgHandle))!=0) return ret;
+  
+  return (Result)cmdbuf[1];
+}
+
+static Result CFG_SetSecureInfo(const u8 *data, u32 size, const u8 *sig, u32 sigsize)
+{
+  Result ret = 0;
+  u32 *cmdbuf = getThreadCommandBuffer();
+
+  cmdbuf[0] = 0x08110084;
+  cmdbuf[1] = size;
+  cmdbuf[2] = sigsize;
+  cmdbuf[3] = (size << 4) | 10;
+  cmdbuf[4] = (u32)data;
+  cmdbuf[5] = (sigsize << 4) | 10;
+  cmdbuf[6] = (u32)sig;
+
+  if((ret = svcSendSyncRequest(cfgHandle))!=0) return ret;
+  
+  return (Result)cmdbuf[1];
+}
+
+static Result CFG_VerifySigSecureInfo(void)
+{
+  Result ret = 0;
+  u32 *cmdbuf = getThreadCommandBuffer();
+
+  cmdbuf[0] = 0x08130000;
+
+  if((ret = svcSendSyncRequest(cfgHandle))!=0) return ret;
+  
+  return (Result)cmdbuf[1];
+}
+
+static Result CFG_DeleteCreateNANDSecureInfo(void)
+{
+  Result ret = 0;
+  u32 *cmdbuf = getThreadCommandBuffer();
+
+  cmdbuf[0] = 0x08120000;
+
+  if((ret = svcSendSyncRequest(cfgHandle))!=0) return ret;
+  
+  return (Result)cmdbuf[1];
+}
 
 static Result FSUSER_ExportIntegrityVerificationSeed(u8 *data)
 {
@@ -82,8 +168,8 @@ static void DoImportSeed(void)
   }
   else
   {
-    FSUSER_ImportIntegrityVerificationSeed(seed);
-    printf("Import seed done.\n");
+    res = FSUSER_ImportIntegrityVerificationSeed(seed);
+    printf("FSUSER_ImportIntegrityVerificationSeed: 0x%08X\n", res);
   }
   fclose(file);
 }
@@ -96,6 +182,39 @@ static void DoFormatCTRNand(void)
   printf("FSUSER_InitializeCtrFileSystem: 0x%08X\n", res);
 
   return;
+}
+
+static void DoImportSecureInfo(void)
+{
+  FILE *file;
+  Result res;
+  u8 secureinfo[0x111];
+
+  if ((file = fopen("sdmc:/SecureInfo_A", "rb")) == NULL)
+  {
+    printf("Cannot find SecureInfo_A\n");
+    return;
+  }
+  if (fread(secureinfo, sizeof(secureinfo), 1, file) < 1)
+  {
+    printf("Error reading secure info\n");
+    fclose(file);
+    return;
+  }
+  fclose(file);
+
+  res = CFG_SetSecureInfo(&secureinfo[0x100], 0x11, secureinfo, 0x100);
+  printf("CFG_SetSecureInfo: 0x%08X\n", res);
+  if (res != 0) return;
+
+  res = CFG_VerifySigSecureInfo();
+  printf("CFG_VerifySigSecureInfo: 0x%08X\n", res);
+  if (res != 0) return;
+
+  res = CFG_DeleteCreateNANDSecureInfo();
+  printf("CFG_DeleteCreateNANDSecureInfo: 0x%08X\n", res);
+  
+  printf("Finished.\n");
 }
 
 int main(int argc, char** argv)
@@ -113,7 +232,14 @@ int main(int argc, char** argv)
     PatchSrvAccess();
     printf("[%08X] Patched process\n", KernelBackdoor(PatchProcess));
     HB_FlushInvalidateCache(); // Just to be sure!
+    if ((res = srvGetServiceHandle(&cfgHandle, "cfg:i")) != 0)
+    {
+      printf("Error 0x%08X initializing cfg.\n", res);
+      svcSleepThread(10000000000ULL);
+      goto end;
+    }
 
+    printf("Press A to install SecureInfo.\n");
     printf("Press X to export seed.\n");
     printf("Press Y to import seed.\n");
     printf("Press Select to format CTRNAND.\n");
@@ -131,6 +257,10 @@ int main(int argc, char** argv)
         {
             DoFormatCTRNand();
         }
+        if (kDown & KEY_A)
+        {
+            DoImportSecureInfo();
+        }
         if (kDown & KEY_X)
         {
             DoExportSeed();
@@ -142,6 +272,7 @@ int main(int argc, char** argv)
         gspWaitForVBlank();
     }
 
+end:
     khaxExit();
     hbExit();
     gfxExit();
